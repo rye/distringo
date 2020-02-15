e5af7ea017d293fc4710b003c82880bef9a89c5c
Kristofer Rye <kristofer.rye@gmail.com>
2020-02-15 17:54:32 -0600

    Add comments describing desired behavior
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 #[tokio::main]
 async fn main() {
 	pretty_env_logger::init();
+
+	// GET / => (fs ./public/index.html)
+
+	// GET /[path/to/files] => (fs ./public/[path/to/files])
+
+	// Compose the routes together.
 }
