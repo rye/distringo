fb698feb7bfbb7af2dce41ea2aebae6c45b0cf20
Kristofer Rye <kristofer.rye@gmail.com>
2020-02-28 16:26:41 -0600

    Error: Add Io variant to wrap std::io::Error types
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,16 @@
+use std::io;
 use core::result;
 
 #[derive(Debug)]
 #[non_exhaustive]
-pub enum Error {}
+pub enum Error {
+	Io(io::Error),
+}
 
 pub type Result<T> = result::Result<T, Error>;
+
+impl From<io::Error> for Error {
+	fn from(e: io::Error) -> Error {
+		Self::Io(e)
+	}
+}
