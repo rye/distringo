0cffb304f6377d22164593ca5317dcedf28d51e1
Kristofer Rye <kristofer.rye@gmail.com>
2020-03-16 12:52:58 -0500

    Add controls to the index page
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,34 @@
 L.Control.Uptown = L.Control.extend({
+	onUpdate: function(e) {
+		if (this.state.collapsed) {
+			this.getContainer().classList.add('collapsed');
+		} else {
+			this.getContainer().classList.remove('collapsed');
+		}
+	},
+
+	onClick: function(e) {
+		if (this.state.collapsed) {
+			this.state.collapsed = false;
+		} else {
+			this.state.collapsed = true;
+		}
+
+		this.onUpdate(e);
+	},
+
 	onAdd: function(map) {
 		let element = L.DomUtil.create('div');
+
 		element.classList.add('uptown-controls');
+		element.classList.add('collapsed');
+
+		L.DomEvent.on(element, { click: this.onClick }, this);
+
+		this.state = {
+			collapsed: true,
+		};
+
 		return element;
 	},
 
