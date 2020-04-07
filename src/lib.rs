437633f323db35beb64d8ea2f971e400dc7e0b8e
Kristofer Rye <kristofer.rye@gmail.com>
2020-04-06 19:26:44 -0500

    Dataset: Add doc comment for get_header_for_geoid function
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@ pub trait Dataset<LogicalRecord> {
 	/// Retrieve the logical record corresponding to GeoID `id`
 	fn get_logical_record_number_for_geoid(&self, geoid: &str) -> Result<LogicalRecordNumber>;
 
+	/// Retrieve the GeographicalHeader
 	fn get_header_for_geoid(&self, geoid: &str) -> Result<Box<dyn GeographicalHeader>>;
 }
 
