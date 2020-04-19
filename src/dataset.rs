2b5a9d1062c7e5b04235482fe40564d040124bc2
Kristofer Rye <kristofer.rye@gmail.com>
2020-04-19 10:15:54 -0500

    dataset: Extract into own mod
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

---
+++ src/dataset.rs
@@ -0,0 +1,15 @@
+use crate::error::Result;
+use crate::GeographicalHeader;
+use crate::LogicalRecordNumber;
+
+/// A trait containing behavior expected for datasets
+pub trait Dataset<LogicalRecord> {
+	/// Retrieve the logical record with number `number`
+	fn get_logical_record(&self, number: LogicalRecordNumber) -> Result<LogicalRecord>;
+
+	/// Retrieve the logical record corresponding to GeoID `id`
+	fn get_logical_record_number_for_geoid(&self, geoid: &str) -> Result<LogicalRecordNumber>;
+
+	/// Retrieve the GeographicalHeader
+	fn get_header_for_geoid(&self, geoid: &str) -> Result<Box<dyn GeographicalHeader>>;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -46,17 +46,8 @@ pub trait LogicalRecord {
 	fn number(&self) -> LogicalRecordNumber;
 }
 
-/// A trait containing behavior expected for datasets
-pub trait Dataset<LogicalRecord> {
-	/// Retrieve the logical record with number `number`
-	fn get_logical_record(&self, number: LogicalRecordNumber) -> Result<LogicalRecord>;
-
-	/// Retrieve the logical record corresponding to GeoID `id`
-	fn get_logical_record_number_for_geoid(&self, geoid: &str) -> Result<LogicalRecordNumber>;
-
-	/// Retrieve the GeographicalHeader
-	fn get_header_for_geoid(&self, geoid: &str) -> Result<Box<dyn GeographicalHeader>>;
-}
+mod dataset;
+pub use dataset::*;
 
 pub struct FileBackedLogicalRecord {
 	number: LogicalRecordNumber,
