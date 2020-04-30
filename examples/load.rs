5c82781a35fc46b8b4e3d93ef8c247686d8f81e8
Kristofer Rye <kristofer.rye@gmail.com>
2020-04-29 21:22:22 -0500

    load: Remove leading 0 from decimal constant
    This makes things marginally less confusing.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- examples/load.rs
+++ examples/load.rs
@@ -9,7 +9,7 @@ fn main() -> distringo::Result<()> {
 	let ds = distringo::IndexedDataset::from_packing_list_file("data/in2010.pl.prd.packinglist.txt")?
 		.index()?;
 
-	let logrecno: distringo::LogicalRecordNumber = 0335180;
+	let logrecno: distringo::LogicalRecordNumber = 335180;
 
 	let record = ds.get_logical_record(logrecno)?;
 	assert_eq!(
@@ -21,11 +21,11 @@ fn main() -> distringo::Result<()> {
 	);
 
 	let logrecno = ds.get_logical_record_number_for_geoid("181570052001013")?;
-	assert_eq!(logrecno, 0335180);
+	assert_eq!(logrecno, 335180);
 
 	let header = ds.get_header_for_geoid("181570052001013")?;
 	assert_eq!(header.name(), "Block 1013");
-	assert_eq!(header.logrecno(), 0335180);
+	assert_eq!(header.logrecno(), 335180);
 
 	Ok(())
 }
