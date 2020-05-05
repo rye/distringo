a86e3d29cbc0b3da032cc1ae1576e75bd128945a
Kristofer Rye <kristofer.rye@gmail.com>
2020-05-05 09:55:06 -0500

    bench: Add benchmarks for extend impl
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- benches/index/logical_record_position.rs
+++ benches/index/logical_record_position.rs
@@ -13,6 +13,12 @@ pub fn benchmark(c: &mut Criterion) {
 		})
 	});
 
+	c.bench_function("lrpi extend 10", |b| {
+		b.iter(|| {
+			lrpi.extend((1..=10).map(|n| (n, n)));
+		})
+	});
+
 	c.bench_function("lrpi insert 100", |b| {
 		b.iter(|| {
 			for i in (1..=100).map(|n| (n, n)) {
@@ -21,11 +27,23 @@ pub fn benchmark(c: &mut Criterion) {
 		})
 	});
 
+	c.bench_function("lrpi extend 100", |b| {
+		b.iter(|| {
+			lrpi.extend((1..=100).map(|n| (n, n)));
+		})
+	});
+
 	c.bench_function("lrpi insert 1000", |b| {
 		b.iter(|| {
 			for i in (1..=1000).map(|n| (n, n)) {
 				lrpi.insert(i.0, i.1);
 			}
+		})
+	});
+
+	c.bench_function("lrpi extend 1000", |b| {
+		b.iter(|| {
+			lrpi.extend((1..=1000).map(|n| (n, n)));
 		})
 	});
 }
