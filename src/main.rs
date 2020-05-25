3d40dd7e69b390049d9949281a266e6858f665ff
Kristofer Rye <kristofer.rye@gmail.com>
2020-05-24 19:19:36 -0500

    server: Clone the Arc for shapefiles to enable usage across threads
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,50 @@
 use config::Config;
 use hyper::StatusCode;
 use log::warn;
+use std::sync::Arc;
 
 use std::net::IpAddr;
 use std::net::SocketAddr;
 
 use warp::{Filter, Rejection, Reply};
 
-fn api_v0() -> warp::filters::BoxedFilter<(impl warp::Reply,)> {
-	let shapefiles_index = warp::get()
-		.and(warp::path::end())
-		.map(|| format!("shapefiles#index"));
-	let shapefiles_show = warp::get()
-		.and(warp::path!(String))
-		.map(|id: String| format!("shapefiles#show (id={:?})", id));
+fn api_v0(cfg: &Config) -> distringo::Result<warp::filters::BoxedFilter<(impl warp::Reply,)>> {
+	let shapefiles: Arc<std::collections::HashMap<String, config::Value>> =
+		Arc::new(cfg.get_table("datasets")?);
+
+	let shapefiles_index = {
+		let shapefiles = shapefiles.clone();
+		// GET /api/v0/shapefiles
+		warp::get()
+			.and(warp::path::end())
+			.map(move || format!("{:?}", shapefiles.clone().keys()))
+	};
+
+	// GET /api/v0/shapefiles/:id
+	let shapefiles_show = {
+		let shapefiles = shapefiles.clone();
+		warp::get().and(warp::path!(String)).map(move |id: String| {
+			format!(
+				"shapefiles#show (id={:?}): {:?}",
+				id,
+				shapefiles.clone().get(&id)
+			)
+		})
+	};
+	// ... /api/v0/shapefiles/...
 	let shapefiles = warp::any()
 		.and(warp::path!("shapefiles" / ..))
 		.and(shapefiles_index.or(shapefiles_show));
 
-	let sessions_index = warp::get()
-		.and(warp::path::end())
-		.map(|| format!("sessions#index"));
-	let sessions_show = warp::get()
-		.and(warp::path!(String))
-		.map(|id: String| format!("sessions#show (id={:?})", id));
-	let sessions = warp::any()
-		.and(warp::path!("sessions" / ..))
-		.and(sessions_index.or(sessions_show));
-
 	let api = warp::path("api");
 	let api_v0 = api.and(warp::path("v0"));
 
-	let gets = shapefiles.or(sessions);
+	let gets = shapefiles;
 
-	warp::any().and(api_v0).and(gets).boxed()
+	Ok(warp::any().and(api_v0).and(gets).boxed())
 }
 
-fn routes() -> warp::filters::BoxedFilter<(impl warp::Reply,)> {
+fn routes(cfg: &Config) -> distringo::Result<warp::filters::BoxedFilter<(impl warp::Reply,)>> {
 	let slash = warp::get()
 		.and(warp::path::end())
 		.and(warp::fs::file("./public/index.html"));
@@ -47,11 +55,13 @@ fn routes() -> warp::filters::BoxedFilter<(impl warp::Reply,)> {
 
 	let files = slash.or(public_files);
 
-	warp::any()
-		.and(api_v0().or(files))
-		.with(warp::log("distringo"))
-		.recover(handle_rejection)
-		.boxed()
+	Ok(
+		warp::any()
+			.and(api_v0(cfg)?.or(files))
+			.with(warp::log("distringo"))
+			.recover(handle_rejection)
+			.boxed(),
+	)
 }
 
 #[tokio::main]
@@ -86,7 +96,7 @@ async fn main() -> distringo::Result<()> {
 		SocketAddr::new(host, port)
 	};
 
-	warp::serve(routes()).run(socket).await;
+	warp::serve(routes(&settings)?).run(socket).await;
 
 	Ok(())
 }
