ab662b6995e857af5c2025d272dd1e88763e481e
Kristofer Rye <kristofer.rye@gmail.com>
2020-05-26 11:44:19 -0500

    server: Wire up shapefile response handlers into their own thing
    This allows me to more tightly encapsulate the information and such.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,52 @@ pub mod routes {
 			use std::sync::Arc;
 			use warp::Filter;
 
+			pub mod shapefiles {
+				use std::sync::Arc;
+
+				pub fn index(
+					shapefiles: &Arc<std::collections::HashMap<String, config::Value>>,
+				) -> impl warp::Reply {
+					warp::reply::json(&shapefiles.keys().collect::<Vec<&String>>())
+				}
+
+				// TODO(rye): Change signature: shapefile: &Shapefile -> http::Response<String>
+				pub fn show(
+					shapefiles: &Arc<std::collections::HashMap<String, config::Value>>,
+					id: &String,
+				) -> hyper::Response<String> {
+					if let Some(config) = shapefiles.get(id) {
+						// let table = config.into_table()?;
+
+						// TODO(rye): load the data
+						// TODO(rye): just keep an in-memory stash of the shapefiles and refute booting if files in config don't exist? (better)
+						let data: String = "{}".to_string();
+
+						let response = {
+							http::response::Builder::new()
+								.status(hyper::StatusCode::OK)
+								.header("Content-Type", "application/vnd.geo+json")
+								// TODO(rye): Clean up error path
+								.body(data)
+								.unwrap()
+						};
+
+						response
+					} else {
+						log::debug!("{:?}", shapefiles);
+
+						let response = {
+							http::response::Builder::new()
+								.status(hyper::StatusCode::INTERNAL_SERVER_ERROR)
+								.body("{}".to_string())
+								.unwrap()
+						};
+
+						response
+					}
+				}
+			}
+
 			pub fn shapefiles(
 				cfg: &config::Config,
 			) -> distringo::Result<warp::filters::BoxedFilter<(impl warp::Reply,)>> {
@@ -24,14 +70,14 @@ pub mod routes {
 					let shapefiles = shapefiles.clone();
 					warp::get()
 						.and(warp::path::end())
-						.map(move || warp::reply::json(&shapefiles.keys().collect::<Vec<&String>>()))
+						.map(move || shapefiles::index(&shapefiles))
 				};
 				// GET /api/v0/shapefiles/:id
 				let shapefiles_show = {
 					let shapefiles = shapefiles.clone();
-					warp::get().and(warp::path!(String)).map(move |id: String| {
-						format!("shapefiles#show (id={:?}): {:?}", id, shapefiles.get(&id))
-					})
+					warp::get()
+						.and(warp::path!(String))
+						.map(move |id: String| shapefiles::show(&shapefiles, &id))
 				};
 				// ... /api/v0/shapefiles/...
 				let shapefiles = warp::any()
