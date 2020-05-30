73bc14dac13a28ce51106b0ad4494370505e4cb8
Kristofer Rye <kristofer.rye@gmail.com>
2020-05-30 16:16:08 -0500

    server: Move server module into mod.rs file under server/
    This helps make it slightly more clear, I suppose, that these files are
    not exposed under the crate's hierarchy.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -11,13 +11,13 @@ async fn handle_rejection(
 ) -> core::result::Result<impl warp::Reply, core::convert::Infallible> {
 	if err.is_not_found() {
 		Ok(warp::reply::with_status(
-			warp::reply::html(include_str!("404.html")),
+			warp::reply::html(include_str!("../404.html")),
 			http::StatusCode::NOT_FOUND,
 		))
 	} else {
 		log::warn!("unhandled rejection: {:?}", err);
 		Ok(warp::reply::with_status(
-			warp::reply::html(include_str!("500.html")),
+			warp::reply::html(include_str!("../500.html")),
 			http::StatusCode::INTERNAL_SERVER_ERROR,
 		))
 	}
