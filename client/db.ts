88a6ef8ed7140bc61779d35b33eb9e0e7a3595bf
Kristofer Rye <kristofer.rye@gmail.com>
2021-02-24 21:39:52 -0600

    client/db: Implement core shapefile loading and saving logic
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- client/db.ts
+++ client/db.ts
@@ -1,22 +1,39 @@
-import { openDB, deleteDB, wrap, unwrap } from 'idb';
+import { openDB, deleteDB, wrap, unwrap, DBSchema, IDBPDatabase, IDBPTransaction } from 'idb'
 
-export const DB = async (name: string, version: number) => {
-	await openDB(name, version, {
-		upgrade(db: unknown, oldVersion: number, newVersion: number, transaction: unknown) {
+import type { Shapefile } from './index'
+
+interface DistringoDB extends DBSchema {
+	shapefiles: {
+		key: string,
+		value: Shapefile,
+	}
+}
+
+export const DB = async (name: string, version: number) => await openDB<DistringoDB>(name, version, {
+	upgrade(db: IDBPDatabase<DistringoDB>, oldVersion: number, newVersion: number, transaction: unknown) {
+		console.debug(`Upgrading db ${name} from v${oldVersion} to v${newVersion}`)
+
+		if (oldVersion === 0) {
+			console.debug(`Initializing database ${name}...`)
+
+			db.createObjectStore('shapefiles', {
+				keyPath: 'id',
+			});
+		} else {
 			switch ([oldVersion, newVersion]) {
 				default:
 					console.error(`don't know how to upgrade from version ${oldVersion} to ${newVersion}`);
 			}
-		},
+		}
+	},
 
-		blocked() {
+	blocked() {
 
-		},
+	},
 
-		blocking() {
-		},
+	blocking() {
+	},
 
-		terminated() {
-		},
-	})
-};
+	terminated() {
+	},
+});
