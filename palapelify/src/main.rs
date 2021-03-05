5689cad5e2f006bd56d4182715deab7b9d5f0714
Kristofer Rye <kristofer.rye@gmail.com>
2021-03-05 06:58:35 -0600

    palapelify: Drastically improve performance by using geo::Intersects
    Rather than manually converting (and doing oodles of f64 -> u32
    conversions), just keep things as they are and use geo::Intersects.
    
    Future improvements may be found by e.g. collecting all points in one
    big collection, but that seems less efficient than this.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- palapelify/src/main.rs
+++ palapelify/src/main.rs
@@ -4,96 +4,6 @@ use std::io::Read;
 use geo_types::CoordNum;
 use itertools::Itertools;
 
-#[derive(Debug, Copy, Clone, Hash, Eq, PartialEq, PartialOrd)]
-struct ApproxGeoDegree(i32);
-
-impl From<ApproxGeoDegree> for f64 {
-	fn from(a: ApproxGeoDegree) -> f64 {
-		(a.0 as f64) / 1_000_000_f64
-	}
-}
-
-impl num_traits::Num for ApproxGeoDegree {
-	type FromStrRadixErr = std::num::ParseFloatError;
-
-	fn from_str_radix(str: &str, radix: u32) -> Result<Self, Self::FromStrRadixErr> {
-		Ok(str.parse::<f64>()?.into())
-	}
-}
-
-impl core::ops::Mul for ApproxGeoDegree {
-	type Output = Self;
-	fn mul(self, other: Self) -> Self {
-		(f64::from(self) * f64::from(other)).into()
-	}
-}
-
-impl core::ops::Add for ApproxGeoDegree {
-	type Output = Self;
-	fn add(self, other: Self) -> Self {
-		(f64::from(self) + f64::from(other)).into()
-	}
-}
-
-impl core::ops::Rem for ApproxGeoDegree {
-	type Output = Self;
-	fn rem(self, other: Self) -> Self {
-		(f64::from(self) % f64::from(other)).into()
-	}
-}
-
-impl core::ops::Div for ApproxGeoDegree {
-	type Output = Self;
-	fn div(self, other: Self) -> Self {
-		(f64::from(self) / f64::from(other)).into()
-	}
-}
-
-impl core::ops::Sub for ApproxGeoDegree {
-	type Output = Self;
-	fn sub(self, other: Self) -> Self {
-		(f64::from(self) - f64::from(other)).into()
-	}
-}
-
-impl num_traits::One for ApproxGeoDegree {
-	fn one() -> Self {
-		Self(1_i32)
-	}
-}
-
-impl num_traits::Zero for ApproxGeoDegree {
-	fn zero() -> Self {
-		Self(0_i32)
-	}
-
-	fn is_zero(&self) -> bool {
-		*self == ApproxGeoDegree::zero()
-	}
-}
-
-impl num_traits::ToPrimitive for ApproxGeoDegree {
-	fn to_i64(&self) -> Option<i64> {
-		todo!()
-	}
-	fn to_u64(&self) -> Option<u64> {
-		todo!()
-	}
-}
-
-impl num_traits::cast::NumCast for ApproxGeoDegree {
-	fn from<T>(pri: T) -> Option<Self> {
-		todo!()
-	}
-}
-
-impl From<f64> for ApproxGeoDegree {
-	/// Convert a float with at most 6 decimal points of precision to this type.
-	fn from(f: f64) -> Self {
-		Self((f * 1_000_000.0_f64) as i32)
-	}
-}
-
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum OverlapType {
 	None,
@@ -141,7 +51,7 @@ fn main() {
 		.map(|(k, v)| (k, (v.geometry).as_ref().expect("geometry-less feature?!")))
 		.collect();
 
-	let features: HashMap<&str, geo::LineString<ApproxGeoDegree>> = features
+	let features: HashMap<&str, geo::LineString<f64>> = features
 		.into_iter()
 		.map(|(k, v)| {
 			let geometry: &geojson::Geometry = v;
@@ -151,8 +61,8 @@ fn main() {
 				.try_into()
 				.expect("failed to convert geometry");
 
-			// TODO Replace clone() with iter constructing ApproxGeoDegrees directly from the reference.
-			let ls: geo::LineString<ApproxGeoDegree> = match geo_geometry {
+			// TODO Replace clone() with iter constructing f64s directly from the reference.
+			let ls: geo::LineString<f64> = match geo_geometry {
 				geo::Geometry::Polygon(p) => p.exterior().clone(),
 				geo::Geometry::MultiPolygon(ps) => geo::LineString(
 					ps.iter()
@@ -164,19 +74,19 @@ fn main() {
 					"while processing {}: Geometry variant {:?} not yet supported",
 					k, geo_geometry
 				),
-			}
-			.into_iter()
-			.map(|f: geo_types::Coordinate<f64>| geo_types::Coordinate {
-				x: ApproxGeoDegree::from(f.x),
-				y: ApproxGeoDegree::from(f.y),
-			})
-			.collect();
+			};
 
 			(k, ls)
 		})
 		.collect();
 
-	println!("Processing {} features", features.len());
+	let feature_count = features.len();
+
+	println!(
+		"Processing {} features ({} pairs)",
+		feature_count,
+		(feature_count * (feature_count - 1)) as f64 / 2.0
+	);
 
 	let pair_overlap_types: HashMap<(&str, &str), OverlapType> = features
 		.iter()
@@ -184,24 +94,18 @@ fn main() {
 		.map(|pair| {
 			let first_ls = pair[0].1;
 			let last_ls = pair[1].1;
-			let first_ls_pts: HashSet<geo_types::Point<ApproxGeoDegree>> =
-				first_ls.clone().into_points().into_iter().collect();
-
-			let last_ls_pts: HashSet<geo_types::Point<ApproxGeoDegree>> =
-				last_ls.clone().into_points().into_iter().collect();
-
-			let pts_count = first_ls_pts.len();
-
-			let intersection = first_ls_pts.intersection(&last_ls_pts);
 
 			let name_a: &str = *pair[0].0;
 			let name_b: &str = *pair[1].0;
 
-			let overlap_type: OverlapType = match intersection.count() {
-				0 => OverlapType::None,
-				1 => OverlapType::SinglePoint,
-				c if c == pts_count => OverlapType::Full,
-				_ => OverlapType::Segment,
+			let overlap_type: OverlapType = {
+				use geo::algorithm::intersects::Intersects;
+
+				if first_ls.intersects(last_ls) {
+					OverlapType::Segment
+				} else {
+					OverlapType::None
+				}
 			};
 
 			((name_a, name_b), overlap_type)
@@ -209,6 +113,8 @@ fn main() {
 		.collect();
 
 	for ((feature_a, feature_b), t) in pair_overlap_types {
-		if t != OverlapType::None {}
+		if t != OverlapType::None {
+			println!("Features {} and {} overlap.", feature_a, feature_b);
+		}
 	}
 }
