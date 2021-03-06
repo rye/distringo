0f172541e2e193b1089a4c557d82ad9d1566b649
Kristofer Rye <kristofer.rye@gmail.com>
2021-03-05 18:55:17 -0600

    palapelify: Use Geometry itself
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- palapelify/src/main.rs
+++ palapelify/src/main.rs
@@ -3,7 +3,7 @@ use std::io::Read;
 
 use itertools::Itertools;
 
-fn feature_to_exterior(feature: &geojson::Feature) -> (&str, geo::LineString<f64>) {
+fn feature_to_geometry(feature: &geojson::Feature) -> (&str, geo::Geometry<f64>) {
 	let feature_name: &str = feature.property("GEOID10").unwrap().as_str().unwrap();
 	let geometry: &geojson::Geometry = (feature.geometry)
 		.as_ref()
@@ -11,27 +11,31 @@ fn feature_to_exterior(feature: &geojson::Feature) -> (&str, geo::LineString<f64
 
 	use core::convert::TryInto;
 
-	let geo_geometry: geo::Geometry<f64> = (geometry.value.clone())
+	let geometry: geo::Geometry<f64> = (geometry.value)
+		.to_owned()
 		.try_into()
 		.expect("failed to convert geometry");
 
-	// TODO Replace clone() with iter constructing f64s directly from the reference.
-	let ls: geo::LineString<f64> = match geo_geometry {
-		geo::Geometry::Polygon(p) => p.exterior().clone(),
-		geo::Geometry::MultiPolygon(ps) => geo::LineString(
-			ps.iter()
-				.map(|p| p.exterior().points_iter().map(Into::into))
-				.flatten()
-				.collect::<Vec<_>>(),
-		),
-		_ => panic!(
-			"while processing {}: Geometry variant {:?} not yet supported",
-			feature_name, geo_geometry
-		),
-	};
-
-	(feature_name, ls)
+	(feature_name, geometry)
 }
+
+const fn name_for_geometry(geometry: &geo::Geometry<f64>) -> &'static str {
+	match geometry {
+		geo::Geometry::MultiPolygon(_) => "MultiPolygon",
+		geo::Geometry::Polygon(_) => "Polygon",
+		geo::Geometry::GeometryCollection(_) => "GeometryCollection",
+		geo::Geometry::Line(_) => "Line",
+		geo::Geometry::Point(_) => "Point",
+		geo::Geometry::LineString(_) => "LineString",
+		geo::Geometry::MultiPoint(_) => "MultiPoint",
+		geo::Geometry::MultiLineString(_) => "MultiLineString",
+		geo::Geometry::Rect(_) => "Rect",
+		geo::Geometry::Triangle(_) => "Triangle",
+	}
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
+struct Timing<'a>(std::time::Duration, &'a str, &'a str, &'a str, &'a str);
 
 fn main() {
 	let input_file: String = std::env::args().nth(1).expect("missing input file name");
@@ -52,27 +56,39 @@ fn main() {
 
 	let data: geojson::FeatureCollection = match data {
 		geojson::GeoJson::FeatureCollection(fc) => fc,
-		_ => unreachable!(),
+		_ => panic!(""),
 	};
 
 	let features: &Vec<geojson::Feature> = &data.features;
 
+	let mut ctr = 0;
+
+	let mut timings: BTreeSet<Timing> = BTreeSet::new();
+
 	let pair_results: HashMap<(&str, &str), bool> = features
 		.iter()
-		.map(feature_to_exterior)
-		.combinations(2)
-		.map(|pair| {
-			let name_a: &str = pair[0].0;
-			let name_b: &str = pair[1].0;
+		.map(feature_to_geometry)
+		.tuple_combinations()
+		.map(|(pair_a, pair_b)| {
+			let name_a: &str = pair_a.0;
+			let name_b: &str = pair_b.0;
+
+			if ctr % 1000 == 0 {
+				// println!("Processing pair #{}", ctr);
+			}
+
+			ctr += 1;
+
+			let t0 = std::time::Instant::now();
 
 			let overlaps: bool = {
 				use geo::bounding_rect::BoundingRect;
 				use geo::intersects::Intersects;
 
-				let ls_a: &geo::LineString<f64> = &pair[0].1;
-				let ls_b: &geo::LineString<f64> = &pair[1].1;
+				let ls_a = &pair_a.1;
+				let ls_b = &pair_b.1;
 
-				match (ls_a.bounding_rect(), ls_b.bounding_rect()) {
+				let res = match (ls_a.bounding_rect(), ls_b.bounding_rect()) {
 					// In nearly all cases, we should have bounding boxes, so check that they
 					// overlap before doing the (more intense) operation of checking each segment
 					// in a LineString for intersection.
@@ -80,7 +96,21 @@ fn main() {
 					// Fall back on simple LineString intersection checking if we couldn't figure
 					// out bounding boxes (e.g. because of an empty LineString? this should be rare.)
 					_ => ls_a.intersects(ls_b),
-				}
+				};
+
+				let t = std::time::Instant::now();
+
+				let timing: Timing = Timing(
+					t.duration_since(t0),
+					name_a,
+					name_b,
+					name_for_geometry(ls_a),
+					name_for_geometry(ls_b),
+				);
+
+				timings.insert(timing);
+
+				res
 			};
 
 			((name_a, name_b), overlaps)
@@ -89,7 +119,18 @@ fn main() {
 
 	for ((name_a, name_b), overlaps) in pair_results {
 		if overlaps {
-			println!("Features {} and {} overlap.", name_a, name_b);
+			// println!("Features {} and {} overlap.", name_a, name_b);
 		}
+	}
+
+	for timing in timings {
+		println!(
+			"{},{},{},{},{}ns",
+			timing.1,
+			timing.2,
+			timing.3,
+			timing.4,
+			timing.0.as_nanos()
+		);
 	}
 }
