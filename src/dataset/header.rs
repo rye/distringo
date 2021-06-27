7efd02c67fcf637281e787aa34dc9358e2545c10
Kristofer Rye <kristofer.rye@gmail.com>
2021-06-27 14:01:10 -0500

    dataset: Split the Census2010/PL94-171-specific code into mod
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/dataset/header.rs
+++ src/dataset/header.rs
@@ -1,1 +1,11 @@
-
+/// Fields in a dataset's Geographical Header.
+///
+/// In general, `enum` types make the best `Field`s because they are easy to implement and the
+/// compiler has an easy time checking for exhaustiveness of a `match` and such things.
+///
+/// Importantly, `Field`s must be possible to distinguish from each other, easy to copy, able to be
+/// debugged, and they should also be sized.
+pub trait Field:
+	Sized + core::fmt::Debug + Copy + Clone + PartialEq + Eq + core::hash::Hash
+{
+}
